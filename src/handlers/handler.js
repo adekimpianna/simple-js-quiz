b87f9c2bc2c9408a60982ee8ca34ed4aa221a826
Yana <adek.impianna@gmail.com>
2020-08-22 16:34:52 +0200

    Completed a function to click an answer
    Added a function to select an answer
    Added a listener to the answer options elements

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -2,12 +2,13 @@
 
 console.log('--- loading handler: showQuestion');
 
-window.onload = function showQuestion() {
+
+function showQuestion() {
   debugger;
 
   // access to the question position on DOM for QUESTION
   let questionEl = document.getElementById('question');
-  console.log('questionEl:', questionEl);
+  //console.log('questionEl:', questionEl);
 
   //they process data using program logic functions
   // to get the current question that has been shuffled with math random >>> put inside of logic.js
@@ -16,29 +17,44 @@ window.onload = function showQuestion() {
   
   //they communicate results to the user
   //insert the text (value) from the question (key) from inside of array questions (an array of objects) into DOM
-  console.log('shuffled question:', shuffledForCurrentQuestion.question);
+  //console.log('shuffled question:', shuffledForCurrentQuestion.question);
   questionEl.innerHTML = shuffledForCurrentQuestion.question;
   
   // access to the question position on DOM for ANSWER OPTIONS
   let options = Array.from(document.getElementsByClassName('answ-text'));
-  console.log('answer options:', options);
+  //console.log('answer options:', options);
   
   options.forEach(option => {
     const number = option.dataset['number'];
-    console.log('number:', number);
-    console.log('option text:', shuffledForCurrentQuestion['option'+number]);
+    //console.log('number:', number);
+    //console.log('option text:', shuffledForCurrentQuestion['option'+number]);
     option.innerText = shuffledForCurrentQuestion['option'+number]
   });
 
   availableQuesions.splice(questionIndex, 1);
   if (availableQuesions.length === 0 || questionCounter >= maxQuestions) {
     //go to the end page
-    return window.location.assign('/end.html');
+    return window.location.assign('https://github.com/adekimpianna/simple-js-quiz');
   }
   questionCounter++;
   acceptingAnswers = true;
 
 };
+
+
+function selectAnswer (e) {
+  let options = Array.from(document.getElementsByClassName('answ-text'));
+  
+  acceptingAnswers = false;
+  options.forEach(option => {
+    const selectedOption = e.target;
+    const selectedAnswer = selectedOption.dataset['number'];
+  });
+  
+  showQuestion();
+};
+
+
 
 
 /* handlers define user interactions
@@ -52,28 +68,4 @@ window.onload = function showQuestion() {
 
   they log any important information for developers
 
-
-  ex: 
-
-  // read & process user input from event
-  let x = parseInt(document.querySelector('#value1').value);
-  let y = parseInt(document.querySelector('#value2').value);
-  let op = document.querySelector('#operator').value;
-
-  // pass user input through core logic
-  const result = doMath(op, x, y); // leave this line!
-  
-  // render output to DOM for user
-  document.getElementById('result').innerHTML = result;
-
-  // log user action for developers
-  console.log('\n--- calculate ---');
-  console.log('x:', typeof x, '\n', x);
-  console.log('y:', typeof y, '\n', y);
-  console.log('op:', typeof op, '\n', op);
-  console.log('result:', typeof result, '\n', result);
-
-
-  // return true for the browser
-  return true;
 */
