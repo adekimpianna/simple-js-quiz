d24c9e0776cb9ab6863db9d59d4b32146d486036
Yana <adek.impianna@gmail.com>
2020-08-22 14:11:03 +0200

    Rendered questions onto DOM
    Added question and global variables on data.js
    Created a function to render questions onto DOM on handler.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@
   this file contains a suggestion of what your initial state should include
   feel free to modify, add or remove properties as your team's project evolves
 
-*/
+
 
 const state = {
   // how many questions has the user answered?
@@ -20,25 +20,118 @@ const state = {
   // the questions in the quiz
   questions: [
     {
-      text: 'What are the different ways to declare a JS variable?',
-      answers: [
-        'constant, let, variable',
-        'var, const, let, function',
-        'var, let, const'
-      ],
-      correct: 2,
-      selected: -1,
+      question: 'Based on which culture is The Witcher made from?',
+      optiona: 'Slavic culture',
+      optionb: 'Germanic culture',
+      optionc: 'Nordic culture',
+      optiond: 'Celtic culture',
+      answer: a,
     },
     {
-      text: 'What does `typeof` do?',
-      answers: [
-        'changes the type of a primitive value',
-        'returns a string describing the type of a value',
-        'determines if a value is primitive',
-        'can tell the difference between arrays and objects',
-      ],
-      correct: 1,
-      selected: -1,
+      
     }
   ]
 };
+
+*/
+
+let shuffledForCurrentQuestion = {};
+let acceptingAnswers = false;
+let questionCounter = 0;
+/*
+let score = 0;
+const correctScore = 10;
+*/
+
+let questions = [
+  {
+    question: 'Based on which culture is The Witcher made from?',
+    option1: 'Slavic culture',
+    option2: 'Germanic culture',
+    option3: 'Nordic culture',
+    option4: 'Celtic culture',
+    answer: 1,
+  },
+  {
+    question: 'From which country is the author of The Withcer books, Andrzej Sapkowski, originally from?',
+    option1: 'Slovenia',
+    option2: 'Germany',
+    option3: 'UK',
+    option4: 'Poland',
+    answer: 4,
+  },
+  {
+    question: 'How many series of The Withcer games are availabe now? (Aug, 2020)',
+    option1: 'One serie',
+    option2: 'Two series',
+    option3: 'Three series',
+    option4: 'Four series',
+    answer: 3,
+
+  },
+  {
+    question: 'Who plays as Geralt of Rivia in The Withcer series from Netflix?',
+    option1: 'Henry Cavill',
+    option2: 'Chris Hemsworth',
+    option3: 'Travic Fimell',
+    option4: 'Matt Bomer',
+    answer: 1,
+
+  },
+  {
+    question: 'On Netflix version of The Withcer, which song made by Jaskier that makes Geralt so famous?',
+    option1: 'The Succubus Bait Song',
+    option2: 'The Stars Above The Path',
+    option3: 'The Lion Club Of Cintra',
+    option4: 'Toss A Coin To Your Witcher',
+    answer: 4,
+
+  },
+  {
+    question: 'Even though the age-gap is almost a century, both Yennefer and Ciri were born?',
+    option1: 'in Aedin',
+    option2: 'in Skallige',
+    option3: 'during Belleteyn',
+    option4: 'during Winter in Cintra',
+    answer: 3,
+
+  },
+  {
+    question: 'Why is Ciri capable of doing powerful things, but Geralt is not?',
+    option1: 'Because she learns it from Yennefer',
+    option2: 'Because she has Elven blood',
+    option3: 'Because she has Elder blood',
+    option4: 'None above is correct',
+    answer: 3,
+
+  },
+  {
+    question: 'Who is the powerful mage in The Witcher that has the polymorph ability?',
+    option1: 'Yennefer of Vengerberg',
+    option2: 'Triss Merigold',
+    option3: 'Fringilla Vigo',
+    option4: 'Philippa Eilhart',
+    answer: 4,
+
+  },
+  {
+    question: 'Which monster in The Witcher represents an unwanted child?',
+    option1: 'Striga',
+    option2: 'Botchling',
+    option3: 'Godling',
+    option4: 'None above is correct',
+    answer: 2,
+
+  },
+  {
+    question: 'Which witcher comes from another witcher school than the Wolf School?',
+    option1: 'Ciri',
+    option2: 'Coën',
+    option3: 'Vesemir',
+    option4: 'Leo',
+    answer: 2,
+  },
+];
+
+let availableQuesions = [...questions];
+let maxQuestions = 10;
--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -1,10 +1,45 @@
 'use strict';
 
-console.log('--- loading handler: _');
+console.log('--- loading handler: showQuestion');
 
-const _ = (event) => {
+window.onload = function showQuestion() {
+  debugger;
+
+  // access to the question position on DOM for QUESTION
+  let questionEl = document.getElementById('question');
+  console.log('questionEl:', questionEl);
+
+  //they process data using program logic functions
+  // to get the current question that has been shuffled with math random >>> put inside of logic.js
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  shuffledForCurrentQuestion = availableQuesions[questionIndex];
+  
+  //they communicate results to the user
+  //insert the text (value) from the question (key) from inside of array questions (an array of objects) into DOM
+  console.log('shuffled question:', shuffledForCurrentQuestion.question);
+  questionEl.innerHTML = shuffledForCurrentQuestion.question;
+  
+  // access to the question position on DOM for ANSWER OPTIONS
+  let options = Array.from(document.getElementsByClassName('answ-text'));
+  console.log('answer options:', options);
+  
+  options.forEach(option => {
+    const number = option.dataset['number'];
+    console.log('number:', number);
+    console.log('option text:', shuffledForCurrentQuestion['option'+number]);
+    option.innerText = shuffledForCurrentQuestion['option'+number]
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  if (availableQuesions.length === 0 || questionCounter >= maxQuestions) {
+    //go to the end page
+    return window.location.assign('/end.html');
+  }
+  questionCounter++;
+  acceptingAnswers = true;
 
 };
+
 
 /* handlers define user interactions
 
